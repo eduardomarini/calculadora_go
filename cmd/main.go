eeb1e204423952ddd9eceb794ef27bd9745579e5
eduardomarini <eduardoribeiromarini@gmail.com>
2024-06-25 16:09:03 -0300

    Implementando erros

--- cmd/main.go
+++ cmd/main.go
@@ -14,35 +14,46 @@ func multiplicacao(a, b float64) float64 {
 	return a * b;
 }
 
-func divisao(a, b flaot64) float64 {
-	if b != 0 {
-		return a / b;
-	} else {
-		return fmt.Println("Não é possível dividir por zero!")
+func divisao(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("Não é possível dividir por zero")
 	}
+	return a / b, nil
 }
 
-func operacao (a, b flaot64, op string) float64 {
+func operacao (a, b float64, op string) (float64, error) {
 	switch op {
 	case "+":
-		return soma(a, b)
+		return soma(a, b), nil
 	case "-":
-		return subtracao(a, b)
+		return subtracao(a, b), nil
 	case "*":
-		return multiplicacao(a, b)
+		return multiplicacao(a, b), nil
 	case "/":
-		return divisao(a, b)
+		resultado, err := divisao(a, b)
+		if err != nil {
+			return 0, err
+		}
+		return resultado, nil
+	default:
+		return 0, fmt.Errorf("Operação inválida")
 	}
 }
 
 func main() {
 
 	var a, b float64
+	var op string
 	fmt.Println("Digite a operação desejada (+)(-)(*)(/): ")
-	operacao(a, b, op)
+	fmt.Scanln(&op)
 	fmt.Println("Digite o primeiro número: ")
 	fmt.Scanln(&a)
 	fmt.Println("Digite o segundo número: ")
 	fmt.Scanln(&b)
-	fmt.Println("Resultado:", resultado)
+	resultado, err := operacao(a, b, op)
+	if err != nil {
+		fmt.Println("Erro", err)
+	} else {
+		fmt.Println("Resultado:", resultado)
+	}
 }
